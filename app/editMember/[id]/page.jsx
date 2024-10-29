4ab9d0d9f5e00a4741b1f92254446340be187a51
denieldavenatividad@gmail.com <denieldavenatividad@gmail.com>
2024-10-30 01:07:56 +0800

    test: deploy

--- app/editMember/[id]/page.jsx
+++ app/editMember/[id]/page.jsx
@@ -21,9 +21,9 @@ const EditMemberPage = async ({ params }) => {
   const { id } = params;
   const memberData = await getMemberById(id);
 
-  if (!memberData) {
-    return <div>Error fetching member data</div>;
-  }
+  // if (!memberData) {
+  //   return <div>Error fetching member data</div>;
+  // }
 
   const { member, description } = memberData;
 
@@ -35,3 +35,40 @@ const EditMemberPage = async ({ params }) => {
 };
 
 export default EditMemberPage;
+// import EditMemberForm from "@/components/EditMemberForm";
+
+// const getMemberById = async (id) => {
+//   try {
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/members/${id}`, {
+//       cache: "no-store"
+//     });
+
+//     if (!res.ok) {
+//       throw new Error("Failed to fetch member");
+//     }
+//     const data = await res.json();
+//     return data.member; // Ensure we return the member object
+//   } catch (error) {
+//     console.log(error);
+//     return null;
+//   }
+// };
+
+// const EditMemberPage = async ({ params }) => {
+//   const { id } = params;
+//   const memberData = await getMemberById(id);
+
+//   if (!memberData) {
+//     return <div>Error fetching member data</div>;
+//   }
+
+//   const { member, description } = memberData;
+
+//   return (
+//     <div>
+//       <EditMemberForm id={id} member={member} description={description} />
+//     </div>
+//   );
+// };
+
+// export default EditMemberPage;
